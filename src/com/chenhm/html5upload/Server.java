ada3d291268cc7fe53496d47e17aa71efb5db5cb
chenhm <chenhm@gmail.com>
2014-12-21 21:52:27 +0800

    fix bug

--- src/com/chenhm/html5upload/Server.java
+++ src/com/chenhm/html5upload/Server.java
@@ -95,6 +95,19 @@ public class Server {
 		return sb.toString();
 	}
 
+	private static void outFile(InputStream is, String name, Response resp)
+			throws IOException {
+		Pattern p = Pattern.compile("\\.([^\\s\\.]+)$");
+		Matcher m = p.matcher(name);
+		if (m.find()) {
+			String ext = m.group(1);
+			String mime = getMIME(ext);
+			if (mime != null)
+				resp.type(mime);
+		}
+		IOUtils.copy(is, resp.raw().getOutputStream());
+	}
+
 	public static void main(String[] args) {
 		if (args.length != 2) {
 			help();
@@ -113,100 +126,86 @@ public class Server {
 		get("/*", (req, resp) -> {
 			String name;
 			File f;
-			if ("/".equals(req.pathInfo()) && req.queryParams().size() > 0) {
-				name = req.queryParams().iterator().next();
-				f = new File(Server.class
-						.getResource(staticFileLocation + name).getFile());
-			} else {
-				name = req.pathInfo();
-				f = new File(uploadFileLocation + name);
+
+			try {
+				if ("/".equals(req.pathInfo())
+						&& req.queryParams().size() > 0) {
+					name = req.queryParams().iterator().next();
+					outFile(Server.class.getResourceAsStream(staticFileLocation	+ name), name, resp);
+					return "";
+				} else {
+					name = req.pathInfo();
+					f = new File(uploadFileLocation + name);
+				}
+				if (f.isDirectory()) {
+					lsdir(f, req.pathInfo(), resp);
+				} else if (f.isFile()) {
+					outFile(new java.io.FileInputStream(f), name, resp);
+				} else if (!f.exists()) {
+					System.out.println("File[" + f.getAbsolutePath() + "] not found.");
+					resp.status(404);
+					resp.body("404 Resource not found");
+				}
+			} catch (Exception e) {
+				throw new RuntimeException(e);
 			}
 
-			// System.out.println(f);
+			// resp.redirect("/index.html", 301);
+			return "";
+		});
+
+		post("/", (request, response) -> {
+			boolean isMultipart = ServletFileUpload
+					.isMultipartContent(request.raw());
+			if (isMultipart) {
+				// Create a new file upload handler
+				ServletFileUpload upload = new ServletFileUpload();
+
+				// Parse the request
 				try {
-					if (f.isDirectory()) {
-						lsdir(f, req.pathInfo(), resp);
-					} else if (f.isFile()) {
-						Pattern p = Pattern.compile("\\.([^\\s\\.]+)$");
-						Matcher m = p.matcher(name);
-						if (m.find()) {
-							String ext = m.group(1);
-							String mime = getMIME(ext);
-							if (mime != null)
-								resp.type(mime);
+					FileItemIterator iter = upload
+							.getItemIterator(request.raw());
+					Map<String, String> map = new HashMap<String, String>();
+
+					while (iter.hasNext()) {
+						FileItemStream item = iter.next();
+						String name = item.getFieldName();
+						InputStream stream = item.openStream();
+						if (item.isFormField()) {
+							map.put(name, IOUtils.toString(stream));
+						} else {
+							// Process the input stream
+							RandomAccessFile raf = new RandomAccessFile(
+									uploadFileLocation + map.get("resumableFilename"), "rw");
+
+							// Seek to position
+							raf.seek((Integer.parseInt(map
+									.get("resumableChunkNumber")) - 1)
+									* Long.parseLong(map.get("resumableChunkSize")));
+
+							// Save to file
+							long readed = 0;
+							byte[] bytes = new byte[1024 * 100];
+							for (int r; (r = stream.read(bytes)) != -1;) {
+								raf.write(bytes, 0, r);
+								readed += r;
+							}
+							IOUtils.closeQuietly(raf);
+
+							if (readed != Integer.parseInt(map.get("resumableCurrentChunkSize"))) {
+								halt(206, "Chunk broken");
+							}
+							System.out.println("File[" + map.get("resumableFilename") + "] chunk "
+									+ map.get("resumableChunkNumber") + " saved, total "
+									+ map.get("resumableTotalChunks"));
 						}
-						IOUtils.copy(new java.io.FileInputStream(f), resp.raw()
-								.getOutputStream());
-					} else if (!f.exists()) {
-						resp.status(404);
-						resp.body("404 Resource not found");
 					}
 				} catch (Exception e) {
 					throw new RuntimeException(e);
 				}
-
-				// resp.redirect("/index.html", 301);
-				return "";
-			});
-
-		post("/",
-				(request, response) -> {
-					boolean isMultipart = ServletFileUpload
-							.isMultipartContent(request.raw());
-					if (isMultipart) {
-						// Create a new file upload handler
-						ServletFileUpload upload = new ServletFileUpload();
-
-						// Parse the request
-						try {
-							FileItemIterator iter = upload
-									.getItemIterator(request.raw());
-							Map<String, String> map = new HashMap<String, String>();
-
-							while (iter.hasNext()) {
-								FileItemStream item = iter.next();
-								String name = item.getFieldName();
-								InputStream stream = item.openStream();
-								if (item.isFormField()) {
-									map.put(name, IOUtils.toString(stream));
-								} else {
-									// Process the input stream
-									RandomAccessFile raf = new RandomAccessFile(
-											uploadFileLocation
-													+ map.get("resumableFilename"),
-											"rw");
-
-									// Seek to position
-									raf.seek((Integer.parseInt(map
-											.get("resumableChunkNumber")) - 1)
-											* Long.parseLong(map
-													.get("resumableChunkSize")));
-
-									// Save to file
-									long readed = 0;
-									byte[] bytes = new byte[1024 * 100];
-									for (int r; (r = stream.read(bytes)) != -1;) {
-										raf.write(bytes, 0, r);
-										readed += r;
-									}
-									raf.close();
-
-									if (readed != Integer.parseInt(map
-											.get("resumableCurrentChunkSize"))) {
-										halt(206, "Chunk broken");
-									}
-									System.out.println("File["
-											+ map.get("resumableFilename") + "] chunk "
-											+ map.get("resumableChunkNumber") + " saved, total "
-											+ map.get("resumableTotalChunks"));
-								}
-							}
-						} catch (Exception e) {
-							throw new RuntimeException(e);
-						}
-					}
-					return "Chunk finished";
-				});
+			}
+			return "Chunk finished";
+		});
 
 		exception(RuntimeException.class, (e, request, response) -> {
 			if (e.getCause() instanceof spark.HaltException) {
